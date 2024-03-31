9420bf086d8ec3a15c299d3dd15650b012bb2aa3
Jose Queiroz <118081152+joserrbv@users.noreply.github.com>
2024-03-30 23:02:07 -0400

    criação do backend
    criação para rodar on line

---
+++ DB/db.js
@@ -0,0 +1,22 @@
+mongoose = require("mongoose");
+require("dotenv").config();
+
+async function main() {
+  mongoose.set("strictQuery", true);
+
+  
+  const NAME = process.env.NAME
+  const SENHA = process.env.SENHA
+  
+  
+
+        try {
+            await mongoose.connect(`mongodb+srv://${NAME}:${SENHA}@cluster0.raiai7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)          
+                console.log("conectado ao banco");
+        } catch (error) {
+                console.log("erro de conexão!");
+            }
+}
+
+
+module.exports = main;
