87a391a516a5925cc2892328555df97084413ed0
Jose Queiroz <118081152+joserrbv@users.noreply.github.com>
2024-03-31 23:06:20 -0400

    hash de senha, model user, confirmação de entrada

---
+++ model/user.js
@@ -0,0 +1,13 @@
+const UsuarioSchema = new mongoose.Schema({
+  username: String,
+  email: { type: String, unique: true },
+  password: String,
+  companyName: String,
+  cnpj: String,
+  whatsapp: String,
+  address: String,
+  instagram: String,
+});
+const User = mongoose.model("User", UsuarioSchema);
+
+module.exports = User;
